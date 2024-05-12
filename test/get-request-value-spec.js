36fd7070c9f83c2f0bd5503b222981f7e7ac1389
testudoq <stephenhstewart@gmail.com>
2024-05-13 11:17:48 +1200

    fixed eslint issue

--- test/get-request-value-spec.js
+++ test/get-request-value-spec.js
@@ -1,9 +1,9 @@
 /*global describe, it, expect, require */
 const getRequestValue = require('../src/lib/get-request-value');
 describe('getRequestValue', () => {
-	'use strict';
+
 	it('returns a literal value for _type=literal', () => {
-		expect(getRequestValue({_type: 'literal', value: 'abc'})).toEqual('abc');
+		expect(getRequestValue({ _type: 'literal', value: 'abc' })).toEqual('abc');
 	});
 	it('returns a replicated value up to size for _type=size', () => {
 		expect(getRequestValue({ '_type': 'size', 'size': '5', 'template': 'A' })).toEqual('AAAAA');
